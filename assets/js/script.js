de75513fc56ef51575d0a6248099a39637c7a8a0
Kelerik <vincent.ng39@gmail.com>
2021-11-14 19:09:04 -0500

    automatic colour updating

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,28 @@
 // array of all the scheduled events for each hour of the day
 var timeblockTexts = [];
+// range of hours to display (24h format)
+var hourStart = 0;
+var hourEnd = 23;
 
 var updateTime = function () {
     // display date in the header
     $("#currentDay").text(moment().format("dddd, MMMM Do YYYY"));
     // update the timeblocks to appropriate colours based on current hour
-    // --- TODO
+    for (let hourIndex = hourStart; hourIndex <= hourEnd; hourIndex++) {
+        if (hourIndex < moment().format("H")) {
+            var timeClass = "past";
+        } else if (hourIndex == moment().format("H")) {
+            var timeClass = "present";
+        } else {
+            var timeClass = "future";
+        }
+        $("#hour-" + hourIndex).addClass(timeClass);
+    }
 };
 
-for (let hourIndex = 0; hourIndex <= 23; hourIndex++) {
-    // convert hour to more readable format ("7" becomes "7am")
+// build the timeblock elements
+for (let hourIndex = hourStart; hourIndex <= hourEnd; hourIndex++) {
+    // convert hour to 12h format ("13" becomes "1pm")
     var hourText = moment(hourIndex, "H").format("ha");
 
     // declare timeblock var for the iterated hour
@@ -20,20 +33,11 @@ for (let hourIndex = 0; hourIndex <= 23; hourIndex++) {
         $('<div class="col-2 pt-3 hour">' + hourText + "</div>")
     );
     // second column: timeblock text area
-    if (hourIndex < moment().format("H")) {
-        var timeClass = "past";
-    } else if (hourIndex == moment().format("H")) {
-        var timeClass = "present";
-    } else {
-        var timeClass = "future";
-    }
     newTimeBlock.append(
         $(
-            '<textarea class="col pt-3 ' +
-                timeClass +
-                '" id=hour-' +
+            '<textarea class="col pt-3" id=hour-' +
                 hourIndex +
-                '" spellcheck="false"></textarea>'
+                ' spellcheck="false"></textarea>'
         )
     );
     // third column: save button
@@ -49,8 +53,9 @@ $(".saveBtn").on("click", function () {
     // get relevant textarea element
     var editedTimeblock = $(this).siblings("textarea");
     // get id without the "hour-" text
+    console.log(editedTimeblock);
     var editedHour = editedTimeblock.attr("id").replace("hour-", "");
-    // read text from the textarea and update the array
+    // read text from the textarea and update the array. using the hour as the index
     timeblockTexts[editedHour] = editedTimeblock.val();
 
     console.log(timeblockTexts);
